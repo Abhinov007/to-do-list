6a763c08b872e638e7d5e2cc089e71218c52d6d2
Abhinov007 <abhinovchakraborty1234@gmail.com>
2022-03-02 16:37:06 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="main div">
+        <div className="centre div">
+          <br />
+          <h1>To Do List</h1>
+          <input type="text" placeholder="Add Items" onChange={} />
+          <button> + </button>
+
+        <ol>
+          <li></li>
+        </ol>
+        </div>
+        
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
